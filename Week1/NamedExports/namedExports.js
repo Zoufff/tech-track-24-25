bfeb3d3a215a3a09b0622a962d545464f728360e
Zoufff <150692238+Zoufff@users.noreply.github.com>
2024-10-18 11:21:01 +0200

    Add files via upload

---
+++ Week1/NamedExports/namedExports.js
@@ -0,0 +1,7 @@
+// Import the mathUtils.js module with the exported functions.
+
+import { square, average } from './mathUtils.js';
+
+// Run both functions and log their output to the console
+console.log(square(3))
+console.log(average(3, 4 , 9))
